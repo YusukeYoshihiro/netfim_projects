5231d26aa05941d5f83fe14159dfb966336d0dd7
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2020-12-15 08:35:37 -0800

    first commit

---
+++ src/containers/header.js
@@ -0,0 +1,20 @@
+import React  from 'react';
+import { Header } from '../components';
+import * as ROUTES from '../constants/routes';
+import Logo from '../Logo_netfilm.png'
+
+
+
+
+export function HeaderContainer ({ children }) {
+   return(
+     <Header>
+       <Header.Frame>
+         <Header.Logo to={ROUTES.HOME} alt="NetFilm" src={Logo} />
+         <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
+       </Header.Frame>
+       {children}
+     </Header>
+   )
+};
+
